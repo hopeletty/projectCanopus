ce0c19dddc281123aef99a997c2f77f470bf95e0
hopeletty <hopeleticia98@gmail.com>
2023-07-12 03:37:45 +0900

    Project Canopus
    Project Canopus

---
+++ js/main.js
@@ -0,0 +1,28 @@
+document.getElementById("registration-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // Perform form validation and submit the data to the server
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirm-password").value;
+    var accountName = document.getElementById("account-name").value;
+    var companyName = document.getElementById("company-name").value;
+    var termsAccepted = document.getElementById("terms").checked;
+    var policyAgreed = document.getElementById("company-policy").checked;
+    var newsletterSubscribed = document.getElementById("newsletter").checked;
+
+    // Perform additional validation as per your requirements
+    // ...
+
+    // Submit the form data to the server
+    // ...
+
+    // Clear the form fields
+    document.getElementById("email").value = "";
+    document.getElementById("password").value = "";
+    document.getElementById("confirm-password").value = "";
+    document.getElementById("account-name").value = "";
+    document.getElementById("company-name").value = "";
+    document.getElementById("terms").checked = false;
+    document.getElementById("company-policy").checked = false;
+    document.getElementById("newsletter").checked = false;
+});
